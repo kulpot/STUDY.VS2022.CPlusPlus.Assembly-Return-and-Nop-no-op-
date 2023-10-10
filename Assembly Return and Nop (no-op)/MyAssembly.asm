2cc1d454c2b8fd4bb44b308177913e96a9cde15b
Kulpot <kulpotwahu@gmail.com>
2023-10-11 06:29:54 +0800

    .....

--- Assembly Return and Nop (no-op)/MyAssembly.asm
+++ Assembly Return and Nop (no-op)/MyAssembly.asm
@@ -14,7 +14,13 @@ doit proc
 	mov eax, 9		; eax has the answer, edx has the remainder
 	mov ebx, 2
 	div ebx
-	ret				; return
+	
+	; 2 useful assembly instrutions: nop , ret
+	nop				; no operation or do nothing for debugging
+	nop
+	nop
+	nop
+	ret				; return 
 	
 	
 	
