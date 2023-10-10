b8fb67ab2af7fa73d13eb1a6bd1d791cf6db1e7f
Kulpot <kulpotwahu@gmail.com>
2023-10-11 06:23:01 +0800

    Add project files.

---
+++ Assembly Return and Nop (no-op)/MyAssembly.asm
@@ -0,0 +1,40 @@
+
+.586
+.model flat, c
+.stack 100h
+.data
+
+.code
+
+doit proc
+
+	; this is a comment i can type whatever i like after a semicolon and assembler will ignore it
+	; 9 / 2
+
+	mov eax, 9		; eax has the answer, edx has the remainder
+	mov ebx, 2
+	div ebx
+	
+	
+	
+	
+	; 8 * 4 
+
+	;mov eax, 8		; eax is accumalator registry
+	;mov ebx, 4
+	;mov edx, 21323	; edx will fill the overflow	; edx is a data registry
+	; mul eax, ebx	; syntacs error: mul only needs 1 argument	
+	;mul ebx
+	;mul 4
+	
+	;mov eax, 0FFFFFFFFh
+	;mov ebx, 0FFFFFFFFh
+	;mul ebx
+
+	; the went bought. some I and, food to store	; wrong syntacs, semantics, parsing
+	; I went to the store, and I bought some food. ;correct semantics, parse
+	; I went to the run, and I bought some dive.	; wrong semantics
+
+doit endp
+
+end
